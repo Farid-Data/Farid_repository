da65c6dbf5a99b24959c21795c4ee5b82dc10252
Farid-Data <F.programming.2002@Gmail.com>
2024-10-25 13:10:54 +0100

    Create child_branch.py

---
+++ child_branch.py
@@ -0,0 +1,8 @@
+print("yes is sounds so cool working as a data scientist")
+x = 10
+numbers = []
+while x > 0 :
+  number = int(input("just insert a number:"))
+  numbers.append(number)
+  x -= 1
+print(numbers)
