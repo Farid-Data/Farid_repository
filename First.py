cacc3485b1fa20bc46878c95312bbc68d44c6f4c
Farid-Data <F.programming.2002@Gmail.com>
2024-10-25 13:00:23 +0100

    Add files via upload
    this is my first project

---
+++ First.py
@@ -0,0 +1,9 @@
+def students_info(name,sId):
+    print(f"Hello {name} with {sId}\nwelcome to our university ")
+
+
+name = (input("hello please insert you name?"))
+sId = int(input("please enter your id numbers"))
+
+students_info(name ,sId)
+
